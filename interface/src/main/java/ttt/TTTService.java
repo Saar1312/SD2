8898967b8dd5cb6e5fd82c5266c1d98c089b3155
saar1312 <saar1312@yahoo.com>
2019-03-14 20:34:30 +0000

    First commit

---
+++ interface/src/main/java/ttt/TTTService.java
@@ -0,0 +1,13 @@
+package ttt;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/** Tic Tac Toe game remote interface. */
+public interface TTTService extends Remote {
+    
+    boolean play(int row, int column, int player) throws RemoteException;
+    int checkWinner() throws RemoteException;
+    String currentBoard() throws RemoteException;
+
+}
